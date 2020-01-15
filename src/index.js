a4190a57378a2a541211d7b4036dbce92b74e1d7
shadow-chadow <56468853+shadow-chadow@users.noreply.github.com>
2020-01-15 19:30:32 +0530

    looks better

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./styles.css";
-import "bootstrap/dist/css/bootstrap.css";
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 ReactDOM.render(<App />, document.getElementById("root"));
